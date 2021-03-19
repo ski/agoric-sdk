517af5a37ba46ea484e4b9ca12a6542c321eb4f8
Dan Connolly <connolly@agoric.com>
2021-03-18 19:42:50 -0500

    feat(SwingSet): export ses-ava test from prepare-test-environment

--- packages/SwingSet/tools/prepare-test-env.js
+++ packages/SwingSet/tools/prepare-test-env.js
@@ -1,13 +1,20 @@
-/* global globalThis */
 /**
  * Prepare Agoric SwingSet vat global environment for testing.
  *
  * installs SES (and does lockdown), plus adds mocks
  * for virtual objects: makeKind, makeWeakStore
  */
+/* global globalThis */
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { wrapTest } from '@agoric/ses-ava';
 import '@agoric/install-ses';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import rawTest from 'ava';
 import { makeFakeVirtualObjectManager } from './fakeVirtualObjectManager';
+
+const test = wrapTest(rawTest);
+export default test;
 
 const { makeKind, makeWeakStore } = makeFakeVirtualObjectManager(3);
 
